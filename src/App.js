24f01b65db53f215c71e77ca5d65802062e1c6e1
Lucas Knudsen <laksteelhouse@gmail.com>
2021-02-05 15:50:22 +0100

    ComputerChoic user story complete

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
 import './App.css';
-
+import React, { useState, useEffect } from 'react'
 
 function App() {
-  
+  const [userChoice, setUserChoice] = useState('')
+  const [computerChoice, setComputerChoice] = useState('')
+
+  useEffect(() => {
+    let result
+    let chance = Math.floor(Math.random() * 3)
+    if (chance === 0) {
+      result = 'scissor'
+    } else if (chance === 1) {
+      result = 'paper'
+    } else {
+      result = 'rock'
+    }
+    setComputerChoice(result)
+  })
+
   return (
     <div>
       <input type="text" />
       <input type="submit" />
       <h3 id="userChoice">You chose: </h3>
-      <h3 id="computerChoice">Computer chose: </h3>
+      <h3 id="computerChoice">Computer chose: {computerChoice}</h3>
       <h3 id="winner">You win?</h3>
     </div>
-    
+
   );
 }
 
