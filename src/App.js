257b0eedb6eb2f4014300d8a151bef9d66e76201
Lucas Knudsen <laksteelhouse@gmail.com>
2021-02-05 16:47:24 +0100

    Solved issue, because function wasnt called with variable. Basic functionality working now. First step done

--- src/App.js
+++ src/App.js
@@ -18,22 +18,40 @@ function App() {
     // setComputerChoice(result)
     return result
   }
-  let iWin
-  const playGame = (userChoice) => {
+
+  const playGame = (user) => {
     let comp = makeComputerChoice()
-    debugger
-    if (userChoice !== 'scissor' || userChoice !== 'rock' || userChoice !== 'paper' ) {
+
+    if (user === "scissor" || user === "rock" || user === "paper") {
+      setComputerChoice(comp);
+      if (comp === user) {
+        document.getElementById("winner").innerHTML = "It's a tie!"
+      } else if (user === "scissor" && comp === "rock") {
+        document.getElementById("winner").innerHTML = "You WIN!"
+      } else if (user === "scissor" && comp === "paper") {
+        document.getElementById("winner").innerHTML = "You LOSE!"
+      } else if (user === "rock" && comp === "paper") {
+        document.getElementById("winner").innerHTML = "You LOSE!"
+      } else if (user === "rock" && comp === "scissor") {
+        document.getElementById("winner").innerHTML = "You WIN!"
+      } else if (user === "paper" && comp === "scissor") {
+        document.getElementById("winner").innerHTML = "You LOSE!"
+      } else if (user === "paper" && comp === "rock") {
+        document.getElementById("winner").innerHTML = "You WIN!"
+      }
+    } else {
       alert('Wrong Word!')
     }
+
   }
 
   return (
     <div>
-      <input type="text" value={userChoice} onChange={(event) => setUserChoice(event.target.value) }/>
-      <input type="submit" onClick={() => playGame() }/>
+      <input type="text" value={userChoice} onChange={(event) => setUserChoice(event.target.value)} />
+      <button type="submit" onClick={() => playGame(userChoice)}>Start Game!</button>
       <h3 id="userChoice">You chose: </h3>
       <h3 id="computerChoice">Computer chose: {computerChoice}</h3>
-      <h3 id="winner">You {iWin}!</h3>
+      <h3 id="winner"></h3>
     </div>
 
   );
