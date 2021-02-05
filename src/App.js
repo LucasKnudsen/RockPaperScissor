79d7df3e0a2f3335d042b715736466fff91cb9ba
Lucas Knudsen <laksteelhouse@gmail.com>
2021-02-05 15:34:12 +0100

    Initial HTML set up. Commencing on JS functionality

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ function App() {
     <div>
       <input type="text" />
       <input type="submit" />
+      <h3 id="userChoice">You chose: </h3>
+      <h3 id="computerChoice">Computer chose: </h3>
+      <h3 id="winner">You win?</h3>
     </div>
     
   );
